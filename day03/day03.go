e7ec725b01e42794dd667c6d7256ad6c1a3e78d1
Timo Ribbers <timo@ribbers.com>
2021-12-03 09:07:42 +0100

    add day03

---
+++ day03/day03.go
@@ -0,0 +1,114 @@
+package day03
+
+import (
+	"bufio"
+	"os"
+	"strconv"
+)
+
+func CalculateGammaEpsilon(data []uint, length int) uint {
+	var gamma uint = 0
+	var epsilon uint = 0
+
+	if len(data) < 1 {
+		return 0
+	}
+
+	for pos := 0; pos < length; pos++ {
+		ones, zeros := 0, 0
+		for _, datum := range data {
+			if (datum>>pos)&1 == 1 {
+				ones++
+			} else {
+				zeros++
+			}
+		}
+
+		if ones > zeros {
+			gamma = gamma | (1 << pos)
+		} else {
+			epsilon = epsilon | (1 << pos)
+		}
+	}
+
+	return gamma * epsilon
+}
+
+func CalculateGammaEpsilonFile(file *os.File) uint {
+	return CalculateGammaEpsilon(parseFile(file))
+}
+
+func CalculateOxygenCO2(data []uint, length int) uint {
+	mostCommon := data
+	leastCommon := data
+
+	if len(data) < 1 {
+		return 0
+	}
+
+	for pos := length - 1; pos >= 0; pos-- {
+		mostCommon = getMostOrLeastCommon(mostCommon, pos, true)
+		leastCommon = getMostOrLeastCommon(leastCommon, pos, false)
+	}
+
+	return mostCommon[0] * leastCommon[0]
+}
+
+func getMostOrLeastCommon(data []uint, pos int, mostCommon bool) []uint {
+	if len(data) < 2 {
+		return data
+	}
+
+	var filteredData []uint
+	ones, zeros := 0, 0
+	for _, datum := range data {
+		if (datum>>pos)&1 == 1 {
+			ones++
+		} else {
+			zeros++
+		}
+	}
+
+	var filterCharAtPos uint
+
+	if mostCommon {
+		if ones >= zeros {
+			filterCharAtPos = 1
+		} else {
+			filterCharAtPos = 0
+		}
+	} else {
+		if ones < zeros {
+			filterCharAtPos = 1
+		} else {
+			filterCharAtPos = 0
+		}
+	}
+
+	for _, datum := range data {
+		if (datum>>uint(pos))&1 == filterCharAtPos {
+			filteredData = append(filteredData, datum)
+		}
+	}
+	return filteredData
+}
+
+func CalculateOxygenCO2File(file *os.File) uint {
+	return CalculateOxygenCO2(parseFile(file))
+}
+
+func parseFile(file *os.File) ([]uint, int) {
+
+	length := 0
+	scanner := bufio.NewScanner(file)
+
+	scanner.Split(bufio.ScanLines)
+	var data []uint
+
+	for scanner.Scan() {
+		num, _ := strconv.ParseUint(scanner.Text(), 2, 32)
+		length = len(scanner.Text())
+		data = append(data, uint(num))
+	}
+	return data, length
+}
---
+++ day03/day03_test.go
@@ -0,0 +1,73 @@
+package day03
+
+import (
+	"fmt"
+	"os"
+	"testing"
+)
+
+func TestCalculateGammaEpsilon(t *testing.T) {
+	data := []uint{
+		0b00100,
+		0b11110,
+		0b10110,
+		0b10111,
+		0b10101,
+		0b01111,
+		0b00111,
+		0b11100,
+		0b10000,
+		0b11001,
+		0b00010,
+		0b01010,
+	}
+	got := CalculateGammaEpsilon(data, 5)
+	if got != 198 {
+		t.Errorf("CalculateGammaEpsilon = %d, want 198\n", got)
+	}
+}
+
+func TestCalculateGammaEpsilonFile(t *testing.T)  {
+	file, err := os.Open("input.txt")
+	if err != nil {
+		fmt.Println("File reading error", err)
+		return
+	}
+
+	got := CalculateGammaEpsilonFile(file)
+
+	fmt.Printf("challenge input data got %d\n", got)
+}
+
+func TestCalculateOxygenCO2(t *testing.T) {
+	data := []uint{
+		0b00100,
+		0b11110,
+		0b10110,
+		0b10111,
+		0b10101,
+		0b01111,
+		0b00111,
+		0b11100,
+		0b10000,
+		0b11001,
+		0b00010,
+		0b01010,
+	}
+	got := CalculateOxygenCO2(data, 5)
+	if got != 230 {
+		t.Errorf("CalculateOxygenCO2 = %d, want 230\n", got)
+	}
+}
+
+func TestCalculateOxygenCO2File(t *testing.T)  {
+	file, err := os.Open("input.txt")
+	if err != nil {
+		fmt.Println("File reading error", err)
+		return
+	}
+
+	got := CalculateOxygenCO2File(file)
+
+	fmt.Printf("challenge input data got %d\n", got)
+}
